f53e37cc6e0ceffa2ff85b00282e1b7392b12441
Lucas Avila <lucasavila@Lucass-MacBook-Pro.local>
2020-03-15 20:09:15 -0300

    Use async/await in the API call

--- test/api/api.spec.js
+++ test/api/api.spec.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
 
-const BASE_URL = "https://api.duckduckgo.com/";
-
 describe('Duck duck go API', () => {
-    it('should print all Icon urls', () => {
-        axios.get(BASE_URL + '?q=simpsons+characters&format=json&pretty=1')
-            .then((response) => {
-                response.data.RelatedTopics.forEach(relatedTopic => {
-                    if(relatedTopic.Icon.URL) {
-                        console.log(relatedTopic.Icon.URL);
-                    }
-                });
-            });
+    const BASE_URL = "https://api.duckduckgo.com/";
+
+    it('should print all Icon urls', async () => {
+        const response = await axios.get(BASE_URL + '?q=simpsons+characters&format=json&pretty=1');
+        response.data.RelatedTopics.forEach(relatedTopic => {
+            if(relatedTopic.Icon.URL) {
+                console.log(relatedTopic.Icon.URL);
+            }
+        });
     });
 });
 
